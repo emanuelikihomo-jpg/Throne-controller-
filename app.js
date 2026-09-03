cdaf1e77817efa45bb8d22353962732a4e5320c3
emanuelikihomo-jpg <emanuelikihomo@gmail.com>
2026-09-04 01:47:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 
+// MT5 Mobile Controller
+const app = {
+  name: "Throne Hub",
+  status: "Disconnected",
+
+  connect() {
+    this.status = "Connected";
+    console.log("MT5 connection requested");
+  },
+
+  buy() {
+    console.log("BUY order requested");
+  },
+
+  sell() {
+    console.log("SELL order requested");
+  },
+
+  closeAll() {
+    console.log("Close all orders requested");
+  }
+};
+
+console.log("Throne Hub MT5 Controller loaded");
