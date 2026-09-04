b35044f32be4dfb8dac00e895ef93818c5dc05ce
emanuelikihomo-jpg <emanuelikihomo@gmail.com>
2026-09-04 02:01:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "online",
+    message: "Throne Hub MT5 Controller is running"
+  });
+});
+
+app.get("/status", (req, res) => {
+  res.json({
+    mt5: "disconnected",
+    message: "MT5 connection will be added next"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Throne Hub server running on port ${PORT}`);
+});
